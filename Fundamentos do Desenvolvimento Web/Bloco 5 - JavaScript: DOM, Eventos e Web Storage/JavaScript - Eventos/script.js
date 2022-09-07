76aa82c35d3f3b257ae75622430af716673aa863
PauloWeyne <you@.com>
2022-09-07 11:24:44 -0300

    exercicio bloco 6.1

--- Fundamentos do Desenvolvimento Web/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/JavaScript - Eventos/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/JavaScript - Eventos/script.js
@@ -13,27 +13,100 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+// Escreva seu código abaixo.
+// Criando os dias com classes e id's:
   let decemberDays = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
 
   function createDaysOfMonth() {
     let decemberDaysOfMonth = document.querySelector('#days');
 
     for (let index = 0; index < decemberDays.length; index += 1) {
+
         let daysMonth = decemberDays[index];
         let daysMonthListItem = document.createElement('li');
         daysMonthListItem.innerHTML = daysMonth;
 
-        decemberDaysOfMonth.appendChild(daysMonthListItem);
+        if (daysMonth === 24 || daysMonth === 31) {
+          daysMonthListItem.className = 'day holiday';
+          decemberDaysOfMonth.appendChild(daysMonthListItem);
+        } else if (daysMonth === 4 || daysMonth === 11 || daysMonth === 18) {
+          daysMonthListItem.className = 'day friday';
+          decemberDaysOfMonth.appendChild(daysMonthListItem);
+        } else if (daysMonth === 25) {
+          daysMonthListItem.className = 'day friday holiday';
+          decemberDaysOfMonth.appendChild(daysMonthListItem);
+        } else {
+          daysMonthListItem.className = 'day';
+          decemberDaysOfMonth.appendChild(daysMonthListItem);
+        }
     }
-}
-createDaysOfMonth();
+  }
+  createDaysOfMonth();
 
-function createLiClass () {
-    let liClass = document.getElementsByTagName('li');
+// Botão para marcar os feriados:
+  function createHolidayButton(buttonName) {
+    let buttonContainer = document.querySelector('.buttons-container');
+    let buttonHoliday = document.createElement('button');
+    let buttonHolidayId = 'btn-holiday';
 
-    for (let index = 9; index <= liClass.length; index += 1) {
-        liClass[index].className = 'day'
-    }
-}
-createLiClass();
+    buttonHoliday.innerHTML = buttonName;
+    buttonHoliday.id = buttonHolidayId;
+
+    buttonContainer.appendChild(buttonHoliday)
+  }
+  createHolidayButton('Feriados');
+
+
+  function displayHoliday() {
+    let getHolidayBtn = document.querySelector('#btn-holiday');
+    let getHolidays = document.querySelectorAll('.holiday');
+    let holidayBackground = 'blue';
+    let setNewColor = 'white';
+
+    getHolidayBtn.addEventListener('click', function(){
+      for (let index = 0; index < getHolidays.length; index += 1) {
+        if (getHolidays[index].style.background === setNewColor){
+          getHolidays[index].style.background = holidayBackground;
+        } else {
+          getHolidays[index].style.background = setNewColor;
+        }
+      }
+    })
+  }
+  displayHoliday();
+
+// Botão para marcar os sextas-feiras:
+  function createFridayButton(buttonName) {
+    let buttonContainer = document.querySelector('.buttons-container');
+    let newButton = document.createElement('button');
+    let newButtonID = 'btn-friday';
+  
+    newButton.innerHTML = buttonName;
+    newButton.id = newButtonID;
+    buttonContainer.appendChild(newButton); 
+  } 
+  createFridayButton('Sexta-feira');
+
+  function displayFridays(fridaysArray) {
+    let getFridayButton = document.querySelector('#btn-friday');
+    let fridays = document.getElementsByClassName('friday');
+    let newFridayText = 'Sextou!';
+  
+    getFridayButton.addEventListener('click', function() {
+    for (let index = 0; index < fridays.length; index += 1) {
+      if (fridays[index].innerHTML !== newFridayText) {
+          fridays[index].innerHTML = newFridayText;
+      } else {
+          fridays[index].innerHTML = fridaysArray[index];
+        }
+      }
+    });
+  }
+
+  let decemberFridays = [ 4, 11, 18, 25 ];
+  displayFridays(decemberFridays);
+
+  
+  
+
+  
