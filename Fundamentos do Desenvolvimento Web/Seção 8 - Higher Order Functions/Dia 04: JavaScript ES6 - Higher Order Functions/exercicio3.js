547a5e656eb8a258b32048cc0209fb21e1e0b18b
PauloWeyne <you@.com>
2022-09-28 13:28:58 -0300

    dia 4 finalizando

--- Fundamentos do Desenvolvimento Web/Seção 8 - Higher Order Functions/Dia 04: JavaScript ES6 - Higher Order Functions/exercicio3.js
+++ Fundamentos do Desenvolvimento Web/Seção 8 - Higher Order Functions/Dia 04: JavaScript ES6 - Higher Order Functions/exercicio3.js
@@ -8,4 +8,16 @@ const names = [
   
   function containsA() {
     // escreva seu código aqui
+    let totalOfLetters = 0;
+    names.forEach((name) => {
+      const lettersFromName = name.split('');
+      totalOfLetters += lettersFromName.reduce((lettersInName, currentLetter) => {
+        if (currentLetter === 'a' || currentLetter === 'A') {
+          return lettersInName + 1;
+        }
+        return lettersInName;
+      }, 0)
+    });
+    return totalOfLetters;
   }
+  console.log(containsA());
