9dcb0c40c56c08fbc36a045080660d8ac42a6460
PauloWeyne <you@.com>
2023-04-18 11:46:58 -0300

    exercicio para fixar dia 4.1

--- back-end/Introdução ao desenvolvimento Web com Node.js/Dia 01: Node.js: Runtime Assíncrono/exercicio/simpsons.js
+++ back-end/Introdução ao desenvolvimento Web com Node.js/Dia 01: Node.js: Runtime Assíncrono/exercicio/simpsons.js
@@ -1,18 +1,23 @@
 const fs = require('fs').promises;
 
-async function getSimpsonsById(id) {
-    const data = await fs.readFile('./simpsons.json', 'utf-8');
-    const simpsons = JSON.parse(data);
-    const chosenSimpson = simpsons.find((element) => Number(element.id) === id)
+// async function getSimpsonsById() {
+//     const data = await fs.readFile('./simpsons.json', 'utf-8');
+//     const dataParse = JSON.parse(data);
+//     const array = dataParse.map(({id, name}) => `${id}: ${name}`);
+//     console.log(array);
+// }
 
-    if(!chosenSimpson) {
-        throw new Error('id não encontrado')
-    } return chosenSimpson
+// getSimpsonsById();
+
+async function writeData() {
+    const fileContent = await fs.readFile('./simpsons.json', 'utf-8')
+    const object = JSON.parse(fileContent)
+    object.push({ id: 10, name: 'Paulo Weyne' })
+
+    const array = object.map(({id, name}) => `${id}: ${name}`)
+    
+
+    console.log(array);
 }
 
-async function main(id) {
-   const simpsons = await getSimpsonsById(id);
-   console.log(simpsons);
-}
-
-main(2)
+writeData()
