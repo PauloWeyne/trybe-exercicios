dd5a0e4c48a5fb755bfd7a4508324839e26286d1
PauloWeyne <you@.com>
2023-04-21 11:14:20 -0300

    Adicionando validação do nome e metodo POST

--- back-end/Introdução ao desenvolvimento Web com Node.js/Dia 04: Node.js: Express e Middlewares/exercicio-1/src/app.js
+++ back-end/Introdução ao desenvolvimento Web com Node.js/Dia 04: Node.js: Express e Middlewares/exercicio-1/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const validadeName = require('./middlewares/validateName');
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/activities', (req, res) => {
+app.post('/activities', validadeName, (req, res) => {
     res.status(201).json({ message: 'Atividade registrada com sucesso' });
 });
 
